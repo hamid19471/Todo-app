178adde5967bf1709237aff2061acd4d9627c806
hamid19471 <hamid.19471@gmail.com>
2023-02-26 22:15:10 +0330

    Update: add state and function and reuse todoform component for edit todos

--- src/Components/TodoList/TodoList.jsx
+++ src/Components/TodoList/TodoList.jsx
@@ -1,18 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import Todo from "../Todo/Todo";
-const TodoList = ({ todos, onDelete, onComplete }) => {
+import TodoForm from "../TodoForm/TodoForm";
+const TodoList = ({ todos, onDelete, onComplete, onUpdate }) => {
+    const [edit, setEdit] = useState({ id: null, title: "", status: false });
+    const handleOnEdit = (editInput) => {
+        onUpdate(edit.id, editInput);
+        setEdit({ id: null, title: "", status: false });
+    };
     return (
         <div className="flex flex-col gap-3">
-            {todos
-                .sort((a, b) => a.id - b.id)
-                .map((item) => (
-                    <Todo
-                        key={item.id}
-                        item={item}
-                        onDelete={() => onDelete(item.id)}
-                        onComplete={() => onComplete(item.id)}
-                    />
-                ))}
+            {!edit.id ? (
+                todos
+                    .sort((a, b) => a.id - b.id)
+                    .map((item) => (
+                        <Todo
+                            key={item.id}
+                            item={item}
+                            onDelete={() => onDelete(item.id)}
+                            onComplete={() => onComplete(item.id)}
+                            onEdit={() => setEdit(item)}
+                        />
+                    ))
+            ) : (
+                <TodoForm submitTodos={handleOnEdit} edit={edit} />
+            )}
         </div>
     );
 };

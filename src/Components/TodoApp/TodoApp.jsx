92711d963169c834d333ee7fa469c33c756e48bd
hamid19471 <hamid.19471@gmail.com>
2023-02-26 22:14:02 +0330

    Update: refactor todoapp component and reduce props

--- src/Components/TodoApp/TodoApp.jsx
+++ src/Components/TodoApp/TodoApp.jsx
@@ -6,11 +6,25 @@ const TodoApp = () => {
     const handleDeleteTodo = (id) => {
         setTodos((prev) => prev.filter((item) => item.id !== id));
     };
+    const handleAddNewTodo = (input) => {
+        const newTodo = {
+            id: Math.floor(Math.random() * 100),
+            title: input,
+            status: false,
+        };
+        setTodos([...todos, newTodo]);
+    };
     const handleCompleteTodo = (id) => {
         const toggleStatus = todos.map((item) =>
             item.id === id ? { ...item, status: !item.status } : item,
         );
         setTodos(toggleStatus);
+    };
+    const handleUpdateTodo = (id, input) => {
+        const editTitle = todos.map((item) =>
+            item.id === id ? { ...item, title: input } : item,
+        );
+        setTodos(editTitle);
     };
     return (
         <div className="flex flex-col items-center gap-5">
@@ -23,12 +37,13 @@ const TodoApp = () => {
                 </span>
             </div>
             <div className="border-4 p-4 border-blue-600 rounded-xl ">
-                <TodoForm todos={todos} setTodos={setTodos} />
+                <TodoForm submitTodos={handleAddNewTodo} />
             </div>
             <TodoList
                 todos={todos}
                 onDelete={handleDeleteTodo}
                 onComplete={handleCompleteTodo}
+                onUpdate={handleUpdateTodo}
             />
         </div>
     );

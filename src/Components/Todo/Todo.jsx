6f824d7c89ca1f891877b0a8c627436167782974
hamid19471 <hamid.19471@gmail.com>
2023-02-26 22:13:27 +0330

    Update: add edit and remove function for todos

--- src/Components/Todo/Todo.jsx
+++ src/Components/Todo/Todo.jsx
@@ -1,14 +1,27 @@
 import React from "react";
 import { PencilSquareIcon } from "@heroicons/react/24/outline";
 import { TrashIcon } from "@heroicons/react/24/outline";
-const Todo = ({ item, onDelete, onComplete }) => {
+const Todo = ({ item, onDelete, onComplete, onEdit }) => {
     return (
         <div className="flex text-white font-semibold text-lg border-2 p-3 rounded-lg border-violet-500 items-center justify-between gap-72">
-            <p>{item.title}</p>
+            <p
+                onClick={onComplete}
+                className={
+                    item.status
+                        ? "cursor-pointer opacity-30 line-through"
+                        : "cursor-pointer opacity-100"
+                }
+            >
+                {item.title}
+            </p>
             <div className="flex gap-4">
                 <button
-                    className="bg-violet-500 p-3 rounded-lg shadow-lg shadow-violet-900 hover:shadow-none hover:scale-105 duration-300"
-                    onClick={onComplete}
+                    onClick={onEdit}
+                    className={
+                        item.status
+                            ? "hidden"
+                            : "bg-violet-500 p-3 rounded-lg shadow-lg shadow-violet-900 hover:shadow-none hover:scale-105 duration-300"
+                    }
                 >
                     <PencilSquareIcon className="h-6 w-6" />
                 </button>

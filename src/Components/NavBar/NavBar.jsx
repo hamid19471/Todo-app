889f4868e20cb28eae6e19d4cf1940e9cd7bb1b4
hamid19471 <hamid.19471@gmail.com>
2023-02-28 22:57:29 +0330

    Update: add select tag for use in filter todos

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,6 +1,15 @@
 import React from "react";
 
-const NavBar = ({ remainingTodo }) => {
+const NavBar = ({
+    remainingTodo,
+    filteringTodo,
+    todoStatus,
+    setTodoStatus,
+}) => {
+    const handleFilterTodos = (event) => {
+        setTodoStatus(event.target.value);
+        filteringTodo(event.target.value);
+    };
     return (
         <div className="text-white">
             <div className="flex gap-3 ">
@@ -9,6 +18,17 @@ const NavBar = ({ remainingTodo }) => {
                     {remainingTodo}
                 </span>
                 Task Remaining
+            </div>
+            <div>
+                <select
+                    value={todoStatus}
+                    onChange={handleFilterTodos}
+                    className="w-full mt-3 p-2 text-slate-900 bg-violet-400 rounded-lg font-bold"
+                >
+                    <option value="All">All</option>
+                    <option value="Completed">Completed</option>
+                    <option value="Uncompleted">Uncompleted</option>
+                </select>
             </div>
         </div>
     );
